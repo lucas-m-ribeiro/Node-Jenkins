fb00f8d5ceb36bf654097d9390c575b606c27cb1
NEXUS <luckas.ribeiro2015@gmail.com>
2019-04-24 15:42:21 -0300

    nodejenkins

---
+++ modulo1.js
@@ -0,0 +1,7 @@
+const modulo1 = {
+    soma: (a) => a+1,
+    sMok: (a, cb) => a+cb(a),
+    mult: (a, b) => a * b,
+    div: (a, b) => a / b,
+}
+module.exports = modulo1
---
+++ modulo1.test.js
@@ -0,0 +1,20 @@
+const modulo1 = require("./modulo1");
+
+describe("basic feature", ()=>{
+    it("Should return the right value", () =>{
+        expect(modulo1.soma(10)).toBe(11)
+    })
+    it("Should return 11", () =>{
+        const cb = jest.fn()
+        cb.mockReturnValue(1)
+        expect(modulo1.sMok(10, cb)).toBe(11)
+    })
+    it("Should return 20", () =>{
+
+        expect(modulo1.mult(10, 2)).toBe(20)
+    })
+    it("Should return 5", () =>{
+
+        expect(modulo1.div(10, 2)).toBe(5)
+    })
+})
